672fbbf442b45d2eeeed889a555fd0b005a8b2d8
ryanbmarx <ryanbmarx@gmail.com>
2018-06-29 16:09:18 -0500

    Update initial tarbell_config.py with default Omniture configuration

--- tarbell_config.py
+++ tarbell_config.py
@@ -39,6 +39,12 @@ S3_BUCKETS = {
 
 # Default template variables
 DEFAULT_CONTEXT = {
+   'OMNITURE': {   'domain': 'chicagotribune.com',
+                    'section': 'news',
+                    'sitename': 'Chicago Tribune',
+                    'subsection': 'local',
+                    'subsubsection': '',
+                    'type': 'dataproject'},
     'name': 'cps-abuse-student-offenders',
     'title': 'CPS Abuse Student Offenders'
 }
